7fefffebb22b75c4184e59d4aa69e497a24bc643
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-09-23 21:48:34 +0200

    Added comments

--- Fractal Tree/fractal.js
+++ Fractal Tree/fractal.js
@@ -1,3 +1,5 @@
+//This fractal tree is based on a recursive definition, where it draws a new tree every iteration
+
 var canvas = document.getElementById('canvas');
 var canvasContext = canvas.getContext('2d');  
 
@@ -6,7 +8,7 @@ Math.radians = function(degrees){
   return degrees * Math.PI / 180;
 };
 
-var depth = 9;
+var depth = 9;          //tree size (how much you see -> depth)
 
 function drawLine(x1, y1, x2, y2, brightness){
   canvasContext.moveTo(x1, y1);
@@ -18,8 +20,8 @@ function drawTree(x1, y1, angle, depth){
     var x2 = x1 + (Math.cos(Math.radians(angle)) * depth * 10.0);     //calculate positions of two branches
     var y2 = y1 + (Math.sin(Math.radians(angle)) * depth * 10.0);
     drawLine(x1, y1, x2, y2, depth);
-    drawTree(x2, y2, angle - 20, depth - 1);
-    drawTree(x2, y2, angle + 20, depth - 1);
+    drawTree(x2, y2, angle - 20, depth - 1);        //left tree, reduce depth (visibility) on every iteration
+    drawTree(x2, y2, angle + 20, depth - 1);        //right tree
   }
 }
 
