5d30c60c2e2cb056e68f2125a0e9c759bb14e025
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-09-25 17:53:40 +0200

    Removed angle-adjusting functionality

--- Fractal Tree/fractal.js
+++ Fractal Tree/fractal.js
@@ -1,9 +1,5 @@
-/*    VERSION 20170924
+/*    VERSION 20170925
 TODO------------------------------------------
-Add a slider for thickness
-(Add a slider for angle)
--->should work, but the fractal doesn't render continuously
-Make the fractal render constantly
 ----------------------------------------------*/
 
 Math.radians = function(degrees){
@@ -13,46 +9,39 @@ Math.degrees = function(radians){
   return radians * 180 / Math.PI;
 };
 
-//var angle = Math.radians(22);
+var angle = Math.radians(22);
 var fps = 30;
 var canvas;
 var canvasContext;
 var stemThickness = 5;
-var angleSlider;
 
 window.onload = function(){
   canvas = document.getElementById('canvas');
   canvasContext = canvas.getContext('2d');
-  setInterval(draw,1000/fps);
+  constructTree(canvas.height/5);
 };
 
-function draw() {
-  angleSlider = parseFloat(document.getElementById('angleSlider').value);
-  console.log(angleSlider);
-  constructTree(canvas.height/5,stemThickness,angleSlider);
-}
-
-function constructTree(l, w, a){
+function constructTree(l){
   colorRect(0,0,canvas.width,canvas.height,'#333')
   canvasContext.translate(canvas.width/2,canvas.height);
-  branch(l, w, a);
+  branch(l, stemThickness);
 
   //fix bug(stem is too thin, exactly half the size it is supposed to be)
   canvasContext.translate(canvas.width/2+w,canvas.height);    //draw a seperate branch 
-  branch(l, w, a);
+  branch(l, stemThickness);
 }
 
-function branch(len, width, angle) {
-  drawLine(0, 0, 0, -len, width, '#ddd');
+function branch(len, thickness) {
+  drawLine(0, 0, 0, -len, thickness, '#ddd');
   canvasContext.translate(0, -len);
   if (len > 3) {
     canvasContext.save();
     canvasContext.rotate(angle);
-    branch(len * 0.75, width-0.75, angle);
+    branch(len * 0.75, thickness-0.75);
     canvasContext.restore();
     canvasContext.save();
     canvasContext.rotate(-angle);
-    branch(len * 0.75, width-0.75, angle);
+    branch(len * 0.75, thickness-0.75);
     canvasContext.restore();
   }
 }
