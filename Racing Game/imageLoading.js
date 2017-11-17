3a5a04203e2709bb830c77e8660e93a376f5f320
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-11-17 20:37:19 +0100

    Improved image loading

--- Racing Game/imageLoading.js
+++ Racing Game/imageLoading.js
@@ -2,10 +2,7 @@ var blueCarPic = document.createElement('img');
 var trackWall = document.createElement('img');
 var trackRoad = document.createElement('img');
 
-//var picsToLoad = 0;		doesn't work because of race condition https://en.wikipedia.org/wiki/Race_condition
-//in this case, the pictures would finish loading before the next one started loading -> counter goes back to zero and game initialises
-//for multiple pictures, this can happen multiple times and the game's logic can run three times as often (setInterval function)
-var picsToLoad = 3;
+var picsToLoad;	//set automatically based length of imageData array in loadImages()
 
 function checkLoadingComplete(){		//if all pictures have loaded game gets started
 	picsToLoad --;
@@ -23,7 +20,15 @@ function beginLoadingImage(imgVar, filePath){
 }
 
 function loadImages(){
-	beginLoadingImage(trackWall, 'track_wall.png');
-	beginLoadingImage(trackRoad, 'track_road.png');
-	beginLoadingImage(blueCarPic, 'blueCarPic.png');
+	var imageData = [									//data structure for storing neccessary image information
+		{var: trackWall,file:'track_wall.png'},			//object literal (=hashtable or =dictionary)
+		{var: trackRoad,file:'track_road.png'},
+		{var: blueCarPic,file:'blueCarPic.png'}
+	];
+
+	picsToLoad = imageData.length;
+
+	for(var i=0;i<imageData.length;i++){
+		beginLoadingImage(imageData[i].var, imageData[i].file);
+	}
 }
