6cdb6e4a92ac777800b0c14e166b84cb962b15af
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-11-17 19:36:27 +0100

    Improve Image-Loading
    Prevent race condition bug too

--- Racing Game/imageLoading.js
+++ Racing Game/imageLoading.js
@@ -2,30 +2,28 @@ var blueCarPic = document.createElement('img');
 var trackWall = document.createElement('img');
 var trackRoad = document.createElement('img');
 
+//var picsToLoad = 0;		doesn't work because of race condition https://en.wikipedia.org/wiki/Race_condition
+//in this case, the pictures would finish loading before the next one started loading -> counter goes back to zero and game initialises
+//for multiple pictures, this can happen multiple times and the game's logic can run three times as often (setInterval function)
 var picsToLoad = 3;
 
 function checkLoadingComplete(){		//if all pictures have loaded game gets started
 	picsToLoad --;
-	console.log(picsToLoad);
+	//console.log(picsToLoad);
 	if(picsToLoad == 0){
-		console.log('game intialising');
+		//console.log('game intialising');
 		initialiseGame();
 	}
 }
 
-function carImageLoad(){
-	blueCarPic.onload = checkLoadingComplete();
-	blueCarPic.src = 'blueCarPic.png';
-}
-
-function trackImageLoad(){
-	trackWall.onload = checkLoadingComplete();
-	trackWall.src = 'track_wall.png';
-	trackRoad.onload = checkLoadingComplete();
-	trackRoad.src = 'track_road.png';
+function beginLoadingImage(imgVar, filePath){
+	//picsToLoad++;
+	imgVar.onload = checkLoadingComplete();
+	imgVar.src = filePath;
 }
 
 function loadImages(){
-	carImageLoad();
-	trackImageLoad();
+	beginLoadingImage(trackWall, 'track_wall.png');
+	beginLoadingImage(trackRoad, 'track_road.png');
+	beginLoadingImage(blueCarPic, 'blueCarPic.png');
 }
