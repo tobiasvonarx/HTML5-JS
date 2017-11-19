852ca937a6b8885039752d21610ff47d3b3637d1
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-11-19 20:48:58 +0100

    Switch cases + neccessary collision improvement
    Used switch cases instead of if statements, added new block types to collision system

--- Racing Game/js/track.js
+++ Racing Game/js/track.js
@@ -3,21 +3,22 @@ const trackCols = 20;
 const trackRows = 15;
 const trackGap = 0;
 const finishLineColors = ['white', 'black'];
-const trackGrid = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
-                 1, 0, 0, 0, 7, 1, 1, 1, 6, 6, 6, 6, 1, 1, 1, 1, 7, 0, 0, 1,
-                 1, 0, 0, 1, 1, 0, 0, 1, 1, 6, 6, 1, 0, 0, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 0, 0, 1, 6, 6, 0, 0, 0, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 7, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 7, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 7, 0, 0, 1, 0, 0, 1, 0, 0, 1,
-                 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 7, 0, 0, 1,
-                 1, 2, 3, 7, 4, 4, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-                 1, 5, 5, 5, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-                 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,
-                 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1];
+const trackGrid = [ 6, 6, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6,
+                    6, 6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    6, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1,
+                    1, 0, 0, 0, 7, 1, 1, 6, 6, 6, 6, 6, 1, 1, 1, 1, 7, 0, 0, 1,
+                    1, 0, 0, 1, 1, 0, 0, 1, 6, 6, 6, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0, 0, 0, 0, 1, 6, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 7, 0, 0, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0, 0, 7, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 7, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+                    1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 7, 0, 0, 1,
+                    1, 2, 3, 7, 4, 4, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+                    1, 5, 5, 5, 0, 0, 1, 6, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 1, 6, 6, 1, 1, 1, 6, 6, 1, 0, 0, 0, 0, 1,
+                    6, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6, 6, 6, 6, 1, 1, 1, 1, 6];
+
 const roadTile = 0;
 const wallTile = 1;
 const treeTile = 6;
@@ -28,73 +29,83 @@ const firstPlayerStartTile = 2;
 const secondPlayerStartTile = 3;
 
 
-function isWallAtColRow(col, row) {
-        if(col >= 0 && col < trackCols &&                       //no bugs
-                row >= 0 && row < trackRows) {
-                var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+function isObstacleAtColRow(col, row) {
+    if(col >= 0 && col < trackCols &&                       //no bugs
+            row >= 0 && row < trackRows) {
+            var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+            var tile = trackGrid[trackIndexUnderCoord];
 
-                if(waymarkReached && (trackGrid[trackIndexUnderCoord]==firstPlayerStartTile||trackGrid[trackIndexUnderCoord]==secondPlayerStartTile)){          //if waymark reached and finish line crossed
-                        finishLineReached = true;
-                        showEndScreen = true;
-                } else if(trackGrid[trackIndexUnderCoord]==waymarkTile){                                                                                                                //if waymark reached
-                        waymarkReached = true;
-                }
+            //anticheat system
+            if(waymarkReached && (tile==firstPlayerStartTile||tile==secondPlayerStartTile)){          //if waymark reached and finish line crossed
+                    finishLineReached = true;
+                    showEndScreen = true;
+            } else if(tile==waymarkTile){                                                                                        //if waymark reached
+                    waymarkReached = true;
+            }
 
-                if(!waymarkReached){
-                        return (trackGrid[trackIndexUnderCoord]==wallTile||trackGrid[trackIndexUnderCoord]==antiCheatTile);
-                } else {                                                                                        //waymark reached
-                        return(trackGrid[trackIndexUnderCoord]==wallTile);
-                }
-        } else {
-                return false;
-        }
+            //check if car hit obstacle
+            if(!waymarkReached){
+                    return (tile==wallTile||tile==antiCheatTile||tile==treeTile||tile==flagTile);
+            } else {                                                                                                            //waymark reached
+                    return(tile==wallTile||tile==treeTile||tile==flagTile);
+            }
+    } else {
+            return false;
+    }
 }
 
 
 
 function carTrackHandling(){
-        var carTrackCol = Math.floor(carX / trackSize);
-        var carTrackRow = Math.floor(carY / trackSize);
-        var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
+    var carTrackCol = Math.floor(carX / trackSize);
+    var carTrackRow = Math.floor(carY / trackSize);
+    var trackIndexUnderCar = rowColToArrayIndex(carTrackCol, carTrackRow);
 
-        if(carTrackCol >= 0 && carTrackCol < trackCols &&                       //no bugs
-                carTrackRow >= 0 && carTrackRow < trackRows) {
+    if(carTrackCol >= 0 && carTrackCol < trackCols &&                       //no bugs
+        carTrackRow >= 0 && carTrackRow < trackRows) {
 
-                if(isWallAtColRow(carTrackCol, carTrackRow)) {                  //wall
-
-                        carX -= Math.cos(carAng) * carSpeed;
-                        carY -= Math.sin(carAng) * carSpeed;
-                        carSpeed *= -0.6;
-                }
+        if(isObstacleAtColRow(carTrackCol, carTrackRow)) {                  //wall
+            carX -= Math.cos(carAng) * carSpeed;
+            carY -= Math.sin(carAng) * carSpeed;
+            carSpeed *= -0.6;
         }
+    }
 }
 
 
 function rowColToArrayIndex(col, row){
-        return col + trackCols * row;           //function to return index of track at a column and a row
+    return col + trackCols * row;           //function to return index of track at a column and a row
 }
 
 function drawTracks(){
-        for(var eachRow=0;eachRow<trackRows;eachRow++) {                        //two for loops to iterate through drawing the cols and rows
-                for(var eachCol=0;eachCol<trackCols;eachCol++) {                //eachRow and eachCol are the loop variables
+    for(var eachRow=0;eachRow<trackRows;eachRow++) {                        //two for loops to iterate through drawing the cols and rows
+        for(var eachCol=0;eachCol<trackCols;eachCol++) {                //eachRow and eachCol are the loop variables
 
-                        var arrayIndex = rowColToArrayIndex(eachCol, eachRow);  //make a variable to map all tracks to an index
-                        
-                        if(trackGrid[arrayIndex]==roadTile){                                                                        //track
-                        		canvasContext.drawImage(trackRoad, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==wallTile){                                                                  //grass
-                           		canvasContext.drawImage(trackWall, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==firstPlayerStartTile || trackGrid[arrayIndex]==secondPlayerStartTile){      //goal
-                                canvasContext.drawImage(trackGoal, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==waymarkTile){
-                        	canvasContext.drawImage(trackRoad, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==antiCheatTile){
-                        	canvasContext.drawImage(trackRoad, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==treeTile){                                                                  //trees
-                            canvasContext.drawImage(trackTree, trackSize*eachCol, trackSize*eachRow);
-                        }else if(trackGrid[arrayIndex]==flagTile){                                                                  //flag
-                            canvasContext.drawImage(trackFlag, trackSize*eachCol, trackSize*eachRow);
-                        }
-                }
+            var arrayIndex = rowColToArrayIndex(eachCol, eachRow);  //make a variable to map all tracks to an index
+            var tile = trackGrid[arrayIndex];
+            var useImage;
+            
+            switch(tile){
+                case roadTile:
+                case waymarkTile:
+                case antiCheatTile:
+                    useImage = trackRoad;
+                    break;
+                case wallTile:
+                    useImage = trackWall;
+                    break;
+                case firstPlayerStartTile:
+                case secondPlayerStartTile:
+                    useImage = trackGoal;
+                    break;
+                case treeTile:
+                    useImage = trackTree;
+                    break;
+                case flagTile:
+                    useImage = trackFlag;
+                    break;
+            }
+            canvasContext.drawImage(useImage, trackSize*eachCol, trackSize*eachRow);
         }
+    }
 }
