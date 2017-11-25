0e68e663c8212a804bf6c122cbcc73cdbaea7f12
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-11-25 21:23:42 +0100

    Now drawing brake and idle states of car differently

--- Racing Game/js/car.js
+++ Racing Game/js/car.js
@@ -17,11 +17,12 @@ const carTurnRate = 0.05;
 const speedDecay = 0.988;		//percent
 const minSpeedToTurn = 0.5;
 
+var previousSpeed;
 
 function carReset(){
 	carAng = Math.radians(270);
-
 	carSpeed = 0;
+	previousSpeed = carSpeed;
 
 	for(var eachRow=0;eachRow<trackRows;eachRow++) {			//two for loops to iterate through drawing the cols and rows
 		for(var eachCol=0;eachCol<trackCols;eachCol++) {		//eachRow and eachCol are the loop variables
@@ -66,5 +67,11 @@ function carMove(){
 }
 
 function drawCar(){
-	drawPicWithRotation(blueCarPic, carX, carY, carAng);
+	//var previousSpeed = carSpeed;
+	if (previousSpeed*speedDecay>carSpeed){									//if car is braking
+		drawPicWithRotation(blueCarPicBrake, carX, carY, carAng);
+	} else {
+		drawPicWithRotation(blueCarPicIdle, carX, carY, carAng);
+	}
+	previousSpeed = carSpeed;
 }
