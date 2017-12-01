4e684fee495f0e964126d2975d8026ea31ca244b
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-12-01 21:49:17 +0100

    made the car a class

--- Racing Game/js/car.js
+++ Racing Game/js/car.js
@@ -1,74 +1,90 @@
-var carX;
-var carY;
-var carAng;
-var carSpeed;
-const carRadius = 5;
-
-
-//booleans for more natural steering
-var keyHeldGas;
-var keyHeldBrakes;
-var keyHeldTurnLeft;
-var keyHeldTurnRight;
-
 const carGasRate = 0.05;
 const carBrakeRate = 0.10;
 const carTurnRate = 0.05;
 const speedDecay = 0.988;		//percent
 const minSpeedToTurn = 0.5;
 
-function carReset(){
-	carAng = Math.radians(270);
-	carSpeed = 0;
-	previousSpeed = carSpeed;
+function carClass(){
+	this.x;
+	this.y;
+	this.ang;
+	this.speed;
 
-	for(var eachRow=0;eachRow<trackRows;eachRow++) {			//two for loops to iterate through drawing the cols and rows
-		for(var eachCol=0;eachCol<trackCols;eachCol++) {		//eachRow and eachCol are the loop variables
+	this.imageBrake;
+	this.imageIdle;
 
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);	//make a variable to map all tracks to an index
+	//booleans for steering
+	this.keyHeldGas;
+	this.keyHeldBrakes;
+	this.keyHeldTurnLeft;
+	this.keyHeldTurnRight;
 
+	this.controlKeyGas;
+	this.controlKeyBrakes;
+	this.controlKeyTurnLeft;
+	this.controlKeyTurnRight;
 
-			if(trackGrid[arrayIndex]==firstPlayerStartTile){		//spawn-point blue car
-				carX = (eachCol * trackSize) + (trackSize/2);
-				carY = (eachRow * trackSize) + (trackSize/2);
+	this.setupKeys = function(keyGas,keyBrakes,keyTurnLeft,keyTurnRight){
+		this.controlKeyGas = keyGas;
+		this.controlKeyBrakes = keyBrakes;
+		this.controlKeyTurnLeft = keyTurnLeft;
+		this.controlKeyTurnRight = keyTurnRight;
+	}
+
+	this.reset = function(startTile, carImageBrake, carImageIdle){
+		this.imageBrake = carImageBrake;
+		this.imageIdle = carImageIdle;
+		this.ang = Math.radians(270);
+		this.speed = 0;
+
+		for(var eachRow=0;eachRow<trackRows;eachRow++) {				//two for loops to iterate through drawing the cols and rows
+			for(var eachCol=0;eachCol<trackCols;eachCol++) {			//eachRow and eachCol are the loop variables
+
+				var arrayIndex = rowColToArrayIndex(eachCol, eachRow);	//make a variable to map all tracks to an index
+
+				if(trackGrid[arrayIndex]==startTile){					//spawn-point
+					this.x = (eachCol * trackSize) + (trackSize/2);
+					this.y = (eachRow * trackSize) + (trackSize/2);
+				}
 			}
 		}
 	}
-}
 
-
-function carMove(){
-	if(carMoved && !finishLineReached){
-		time += 0.0333333;
-	}
-
-	if(keyHeldGas){												//executed every frame the up arrow is held
-		carSpeed += carGasRate;
-	}
-	if(keyHeldBrakes){
-		carSpeed -= carBrakeRate;
-	}
-	if(Math.abs(carSpeed)>minSpeedToTurn){
-		if(keyHeldTurnLeft){
-		carAng -= carTurnRate;
+	this.move = function(){
+		if(carMoved && !finishLineReached){
+			time += 0.0333333;
 		}
-		if(keyHeldTurnRight){
-			carAng += carTurnRate;
+
+		if(this.keyHeldGas){												//executed every frame the up arrow is held
+			this.speed += carGasRate;
+		}
+		if(this.keyHeldBrakes){
+			this.speed -= carBrakeRate;
+		}
+		if(Math.abs(this.speed)>minSpeedToTurn){
+			if(this.keyHeldTurnLeft){
+			this.ang -= carTurnRate;
+			}
+			if(this.keyHeldTurnRight){
+				this.ang += carTurnRate;
+			}
+		}
+		if(!this.keyHeldGas){
+			this.speed *= speedDecay;
+		}
+
+		this.x += Math.cos(this.ang) * this.speed;						//move car
+		this.y += Math.sin(this.ang) * this.speed;
+
+		carTrackHandling(this);
+	}
+
+	this.draw = function(){
+		if (this.keyHeldBrakes){											//if car is braking
+			drawPicWithRotation(this.imageBrake, this.x, this.y, this.ang);	
+		} else {
+			drawPicWithRotation(this.imageIdle, this.x, this.y, this.ang);
 		}
 	}
-	if(!keyHeldGas){
-		carSpeed *= speedDecay;
-	}
 
-	carX += Math.cos(carAng) * carSpeed;						//move car
-	carY += Math.sin(carAng) * carSpeed;
-}
-
-function drawCar(){
-	//var previousSpeed = carSpeed;
-	if (keyHeldBrakes){											//if car is braking
-		drawPicWithRotation(blueCarPicBrake, carX, carY, carAng);	
-	} else {
-		drawPicWithRotation(blueCarPicIdle, carX, carY, carAng);
-	}
 }
