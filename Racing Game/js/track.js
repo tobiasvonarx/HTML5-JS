88e69588ac7f4d5a1403100f9aa535010d8b78df
tobiasvonarx <tobias_vonarx@yahoo.de>
2017-12-02 13:38:56 +0100

    improved collision code and moved it, "leveling" concept

--- Racing Game/js/track.js
+++ Racing Game/js/track.js
@@ -2,7 +2,7 @@ const trackSize = 40;
 const trackCols = 20;
 const trackRows = 15;
 const trackGap = 0;
-const trackGrid = [ 8, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6,
+const levelOne = [ 8, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6,
                     6, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                     8, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
                     1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 6, 1, 1, 1, 1, 1, 0, 0, 0, 1,
@@ -14,9 +14,11 @@ const trackGrid = [ 8, 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6,
                     1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 7, 0, 0, 1, 0, 0, 1, 0, 0, 1,
                     1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 7, 0, 0, 1,
                     1, 2, 3, 7, 4, 4, 1, 6, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
-                    1, 5, 5, 5, 0, 0, 8, 8, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
-                    1, 0, 0, 0, 0, 0, 1, 8, 6, 1, 6, 1, 8, 6, 1, 0, 0, 0, 0, 1,
-                    1, 1, 1, 1, 1, 1, 8, 6, 8, 1, 1, 1, 6, 8, 1, 1, 1, 1, 8, 6];
+                    1, 5, 5, 5, 0, 0, 8, 8, 1, 0, 0, 0, 1, 8, 0, 0, 0, 0, 0, 1,
+                    1, 0, 0, 0, 0, 0, 1, 8, 6, 1, 6, 8, 8, 6, 1, 0, 0, 0, 0, 1,
+                    1, 1, 1, 1, 1, 1, 8, 6, 8, 6, 8, 8, 6, 8, 1, 1, 1, 1, 8, 6];
+
+var trackGrid = [];
 
 const roadTile = 0;
 const wallTile = 1;
@@ -29,32 +31,16 @@ const firstPlayerStartTile = 2;
 const secondPlayerStartTile = 3;
 
 
-function isObstacleAtColRow(car, col, row) {
+function returnTileTypeAtColRow(col, row) {
     if(col >= 0 && col < trackCols &&                       //no bugs
-            row >= 0 && row < trackRows) {
-            var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-            var tile = trackGrid[trackIndexUnderCoord];
-
-            //anticheat system
-            if(car.waymarkReached && (tile==firstPlayerStartTile||tile==secondPlayerStartTile)){          //if waymark reached and finish line crossed
-                    car.finishLineReached = true;
-                    showEndScreen = true;
-            } else if(tile==waymarkTile){                                                                                        //if waymark reached
-                    car.waymarkReached = true;
-            }
-
-            //check if car hit obstacle
-            if(!car.waymarkReached){
-                    return (tile==wallTile||tile==antiCheatTile||tile==treeTile||tile==flagTile);
-            } else {                                                                                                            //waymark reached
-                    return(tile==wallTile||tile==treeTile||tile==flagTile);
-            }
+        row >= 0 && row < trackRows) {
+        var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+        var tile = trackGrid[trackIndexUnderCoord];
+        return tile;
     } else {
-            return false;
+        return wallTile;
     }
 }
-
-
 
 function carTrackHandling(car){
     var carTrackCol = Math.floor(car.x / trackSize);
@@ -64,10 +50,28 @@ function carTrackHandling(car){
     if(carTrackCol >= 0 && carTrackCol < trackCols &&                       //no bugs
         carTrackRow >= 0 && carTrackRow < trackRows) {
 
-        if(isObstacleAtColRow(car, carTrackCol, carTrackRow)) {                  //wall
+        var tile = returnTileTypeAtColRow(carTrackCol, carTrackRow);
+        var hitObstacle;
+
+        //anticheat system
+        if(car.waymarkReached && (tile==firstPlayerStartTile||tile==secondPlayerStartTile)){          //if waymark reached and finish line crossed
+            car.finishLineReached = true;
+            showEndScreen = true;
+        } else if(tile==waymarkTile){                                                                                        //if waymark reached
+            car.waymarkReached = true;
+        }
+
+        //check if car hit obstacle
+        if(!car.waymarkReached){
+            hitObstacle = tile==wallTile||tile==antiCheatTile||tile==treeTile||tile==flagTile||tile==treeTile2;
+        } else {                                                                                                            //waymark reached
+            hitObstacle = tile==wallTile||tile==treeTile||tile==flagTile||tile==treeTile2;
+        }
+
+        if(hitObstacle) {                  //wall
             car.x -= Math.cos(car.ang) * car.speed;
             car.y -= Math.sin(car.ang) * car.speed;
-            car.speed *= -0.5;
+            car.speed *= -0.4;
         }
     }
 }
