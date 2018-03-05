d1da2566afa2f1a3018b1259acd5c36f33d08bda
tobiasvonarx <tobias_vonarx@yahoo.de>
2018-03-05 21:09:53 +0100

    First working version, however inelegant..

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,3 +1,7 @@
+//TODO: top overshooting not flawless
+// 		check for overshooting bugs in general
+//		improve code elegancy and make it more concise
+
 let origBoard;
 let moves;
 const oPlayer = 'O';
@@ -59,30 +63,113 @@ function turn(squareId, player) {
 
 function gameCompleted(id) {
 	let player = origBoard[id];
+	let rCol = [4,9,14,19];
+	let lCol = [0,5,10,15,25];
 	for (let i=0; i<25; i++) {
 		//BOTTOM LEFT DIAGONALS
 		let leftDiagonalA = i+cellCountX-1;
 		let leftDiagonalB = leftDiagonalA+cellCountX-1;
 		let leftDiagonalC = leftDiagonalB+cellCountX-1;
-		//console.log(cell, leftDiagonalA, leftDiagonalB);
 
-		if (origBoard[i]==origBoard[leftDiagonalA]&&origBoard[i]==origBoard[leftDiagonalB]&&origBoard[i]==origBoard[leftDiagonalC]) {
-			showWinner(player);
-		}
 		//BOTTOM RIGHT DIAGONALS
 		let rightDiagonalA = i+cellCountX+1;
 		let rightDiagonalB = rightDiagonalA+cellCountX+1
 		let rightDiagonalC = rightDiagonalB+cellCountX+1;
-		//console.log(cell, rightDiagonalA, rightDiagonalB);
 
-		if (origBoard[i]==origBoard[rightDiagonalA]&&origBoard[i]==origBoard[rightDiagonalB]&&origBoard[i]==origBoard[rightDiagonalC]) {
+		if (typeof origBoard[i]=='string') {
+			//check for LEFT DIAGONAL overshoot
+			//check if a diagonal or the cell itself is on a corner here
+			if (rCol.includes(leftDiagonalA) || rCol.includes(leftDiagonalB) || rCol.includes(leftDiagonalC)) {
+				//check right edges before corners
+				if (rCol.includes(leftDiagonalA)) {
+					leftDiagonalA += cellCountX;
+					leftDiagonalB = leftDiagonalA+cellCountX-1;
+					leftDiagonalC = leftDiagonalB+cellCountX-1;
+				} else if (rCol.includes(leftDiagonalB)) {
+					leftDiagonalB += cellCountX;
+					leftDiagonalC = leftDiagonalB+cellCountX-1;
+				} else if (rCol.includes(leftDiagonalC)) {
+					leftDiagonalC += cellCountX;
+				}
+			}
+			//check if there is an overshoot on the bottom left tile
+			if (i == 20) {
+				leftDiagonalA = 4;
+				leftDiagonalB = leftDiagonalA+cellCountX-1;
+				leftDiagonalC = leftDiagonalB+cellCountX-1;
+			} else if (leftDiagonalA == 20) {
+				leftDiagonalB = 4;
+				leftDiagonalC = leftDiagonalB+cellCountX-1;
+			} else if (leftDiagonalB == 20) {
+				leftDiagonalC = 4;
+			} //check if there is an overshoot on the bottom right tile 
+			else if (i == 24) {
+				leftDiagonalA = 3;
+				leftDiagonalB = leftDiagonalA+cellCountX-1;
+				leftDiagonalC = leftDiagonalB+cellCountX-1;
+			} else if (leftDiagonalA == 24) {
+				leftDiagonalB = 3;
+				leftDiagonalC = leftDiagonalB+cellCountX-1;
+			} else if (leftDiagonalB == 24) {
+				leftDiagonalC = 3;
+			}
+			console.log('cell: ',i);
+			console.log('left diagonals are: ');
+			console.log(leftDiagonalA+' '+leftDiagonalB+' '+leftDiagonalC);
+
+			//check for RIGHT DIAGONAL overshoot
+			//check if a diagonal or the cell itself is on a corner here
+			if (lCol.includes(rightDiagonalA) || lCol.includes(rightDiagonalB) || lCol.includes(rightDiagonalC)) {
+				console.log('edge rightdiagonal');
+				//check left edges before corners
+				if (lCol.includes(rightDiagonalA)) {
+					rightDiagonalA -= cellCountX;
+					rightDiagonalB = rightDiagonalA+cellCountX+1;
+					rightDiagonalC = rightDiagonalB+cellCountX+1;
+				} else if (lCol.includes(rightDiagonalB)) {
+					rightDiagonalB -= cellCountX;
+					rightDiagonalC = rightDiagonalB+cellCountX+1;
+				} else if (lCol.includes(rightDiagonalC)) {
+					rightDiagonalC -= cellCountX;
+				}
+			}
+			//check if there is an overshoot on the bottom left tile
+			if (i == 20) {
+				rightDiagonalA = 1;
+				rightDiagonalB = rightDiagonalA+cellCountX+1;
+				rightDiagonalC = rightDiagonalB+cellCountX+1;
+			} else if (rightDiagonalA == 20) {
+				rightDiagonalB = 1;
+				rightDiagonalC = rightDiagonalB+cellCountX+1;
+			} else if (rightDiagonalB == 20) {
+				rightDiagonalC = 1;
+			} //check if there is an overshoot on the bottom right tile 
+			else if (i == 24) {
+				rightDiagonalA = 0;
+				rightDiagonalB = rightDiagonalA+cellCountX+1;
+				rightDiagonalC = rightDiagonalB+cellCountX+1;
+			} else if (rightDiagonalA == 24) {
+				rightDiagonalB = 0;
+				rightDiagonalC = rightDiagonalB+cellCountX+1;
+			} else if (rightDiagonalB == 24) {
+				rightDiagonalC = 0;
+			}
+
+			console.log('right diagonals are: ');
+			console.log(rightDiagonalA+' '+rightDiagonalB+' '+rightDiagonalC);
+
+		}
+
+		//console.log(cell, rightDiagonalA, rightDiagonalB);
+		if ((origBoard[i]==origBoard[leftDiagonalA]&&origBoard[i]==origBoard[leftDiagonalB]&&origBoard[i]==origBoard[leftDiagonalC])||
+			(origBoard[i]==origBoard[rightDiagonalA]&&origBoard[i]==origBoard[rightDiagonalB]&&origBoard[i]==origBoard[rightDiagonalC])) {
 			showWinner(player);
 		}
 	}
 }
 
 function showWinner(player) {
-	console.log(player+' won the game!');
+	//console.log(player+' won the game!');
 	document.getElementById('winner').innerText = player + ' won the game!';
 	startGame();
 }
