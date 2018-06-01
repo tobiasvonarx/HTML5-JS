0cf647d5b6f7007d823a4610c5e27d6863180ffd
Tobias von Arx <tobias_vonarx@yahoo.de>
2018-06-01 16:00:59 +0200

    Variable learning rate

--- Polynomial Regression/main.js
+++ Polynomial Regression/main.js
@@ -6,8 +6,9 @@ let a, b, c, d;
 let w, h;
 let coefficients;
 
-const learningRate = 0.15;
-const optimizer = tf.train.adam(learningRate);
+let learningRate;
+let lr_slider;
+let optimizer;
 
 let deg_slider;
 let poly_deg;
@@ -16,12 +17,19 @@ function setup() {
 	w = window.innerWidth-20;
 	h = window.innerHeight-20;
 
+	learningRate = 0.15;
+	poly_deg = 0;
+
 	createCanvas(w, h);
+
+	setOptimizer();
 
 	deg_slider = createSlider(10, 30, 10);
 	deg_slider.position(20, 40);
+	lr_slider = createSlider(0, 20, 3);
+	lr_slider.position(160, 40);
 
-	poly_deg = 0;
+
 
 	a = tf.variable(tf.scalar(random(-1, 1)));
 	b = tf.variable(tf.scalar(random(-1, 1)));
@@ -31,6 +39,10 @@ function setup() {
 
 function loss(pred, labels) {
 	return pred.sub(labels).square().mean();
+}
+
+function setOptimizer() {
+	optimizer = tf.train.adam(learningRate)
 }
 
 function predict(x) {
@@ -47,7 +59,7 @@ function predict(x) {
 }
 
 function mousePressed() {
-	if ((mouseX < w && mouseY < h) && (mouseX > 160 || mouseY > 60)) {
+	if ((mouseX < w && mouseY < h) && (mouseX > 300 || mouseY > 60)) {
 		let x = map(mouseX, 0, w, -1, 1);
 		let y = map(mouseY, 0, h, 1, -1);
 		x_vals.push(x);
@@ -61,6 +73,13 @@ function draw() {
 	// polynomial degree gets taken from slider value
 	poly_deg = round(deg_slider.value() / 10);
 
+	// learning rate gets taken from slider value
+	if (learningRate!=lr_slider.value()/20) {
+		learningRate = lr_slider.value() / 20;
+		setOptimizer();
+	}
+	
+	// minimize the loss with the optimizer
 	tf.tidy(() => {
 		if (x_vals.length > 0) {
 			const ys = tf.tensor1d(y_vals);
@@ -68,6 +87,7 @@ function draw() {
 		}
 	});
 
+	// draw the dots
 	stroke('rgba(255, 255, 255, 0.25)');
 	strokeWeight(8);
 	for (let i = 0; i < x_vals.length; i++) {
@@ -76,16 +96,19 @@ function draw() {
 		point(px, py);
 	}
 
+	// instantiate x-intervals of the curve
 	const curveX = [];
 
 	for (let x = -1; x < 1.01; x += 0.025) {
 		curveX.push(x);
 	}
 
+	// get predictions for y-values of the curve
 	const ys = tf.tidy(() => predict(curveX));
 	let curveY = ys.dataSync();
 	ys.dispose();
 
+	// draw the curve
 	noFill();
 	stroke('rgba(255, 255, 255, 0.75)');
 	beginShape();
@@ -96,7 +119,9 @@ function draw() {
 	}
 	endShape();
 
+	// render the text
 	strokeWeight(0.1);
 	fill(200);
 	text('polynomial degree: ' + poly_deg, 20, 20);
+	text('learning rate: ' + learningRate, 160, 20);
 }
